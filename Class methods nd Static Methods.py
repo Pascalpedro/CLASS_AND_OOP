f2cc7d3af5ceb0d195bb8508ba4f6748669b2402
Attama Pascal <Attamapascalpedro@gmail.com>
2022-02-28 12:02:11 +0100

    Add files via upload

---
+++ Class methods nd Static Methods.py
@@ -0,0 +1,130 @@
+"""class Employee:
+
+    num_of_emps = 0
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@gmail.com'
+        self.pay = pay
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * Employee.raise_amt)
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+emp_1 = Employee('Attama', 'Pascalpedro', 50000)
+emp_2 = Employee('Lord', 'Pedro', 60000)
+
+# Employee.set_raise_amt(1.05)
+# OR
+emp_1.set_raise_amt(1.05)
+
+print(Employee.raise_amt)
+print(emp_1.raise_amt)
+print(emp_2.raise_amt)"""
+
+
+
+
+pass
+
+
+
+"""class Employee:
+
+    num_of_emps = 0
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@gmail.com'
+        self.pay = pay
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * Employee.raise_amt)
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str_1.split('-')
+        # return Employee(first, last, pay)
+        # OR
+        return cls(first, last, pay)
+
+emp_1 = Employee('Attama', 'Pascalpedro', 50000)
+emp_2 = Employee('Lord', 'Pedro', 60000)
+
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jane-Doe-90000'
+
+new_emp_1 =Employee.from_string(emp_str_1)
+
+
+print(new_emp_1.email)
+print(new_emp_1.pay)"""
+
+
+
+
+class Employee:
+
+    num_of_emps = 0
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@gmail.com'
+        self.pay = pay
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * Employee.raise_amt)
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str_1.split('-')
+        # return Employee(first, last, pay)
+        # OR
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+emp_1 = Employee('Attama', 'Pascalpedro', 50000)
+emp_2 = Employee('Lord', 'Pedro', 60000)
+
+import datetime
+my_date = datetime.date(2016, 7, 10)
+
+print(Employee.is_workday(my_date))
