6c4e9c39136e2a93dd4acdb0c97f1941eba5f268
Attama Pascal <Attamapascalpedro@gmail.com>
2022-10-17 20:56:38 +0100

    This is the basic Python class syntax

---
+++ Class_snytax.py
@@ -0,0 +1,10 @@
+Class Methodclass:
+    def method():
+        pass
+      
+Class Name_of_class:
+    def __init__():
+        do something.....
+        
+    def function_name(self):
+        do something....
