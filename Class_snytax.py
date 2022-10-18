28140a50ce0c37653653f5caa45ceb489f738387
Attama Pascal <Attamapascalpedro@gmail.com>
2022-10-18 22:26:45 +0100

    Update Class_snytax.py
    another class oop syntax

--- Class_snytax.py
+++ Class_snytax.py
@@ -13,11 +13,24 @@ Class Name_of_class:
 #OR
 
 
-Class NameOfClass():
+"""Class NameOfClass():
     def __init__(self, param1, param2):
         self.param1 = param1
         self.param2 = param2
         
     def some_method(self):
         #perform some action....
-        print(self.param1)
+        print(self.param1)"""
+           
+
+#OR
+
+
+Class Name_of_class:
+    def __init__(arg1, arg2...):
+        self1 = arg1
+        self2= arg2
+        self3 = arg3
+        
+    def __init__(self, mybreed):
+        self.myattribute = mybreed    #but by convention, it is advised to use the same parameter name for all three, (i.e; the arguments should be the same as the attributes).
